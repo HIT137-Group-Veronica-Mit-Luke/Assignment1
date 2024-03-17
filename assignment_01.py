b2f2251bcc61dff79f0fbee5fd63529647fb3307
sigami79 <162386569+sigami79@users.noreply.github.com>
2024-03-17 14:26:37 +0930

    Add files via upload

---
+++ assignment_01.py
@@ -0,0 +1,41 @@
+# Title: How Image processing model saves life from skin cancer.
+
+# 
+
+
+#Introduction
+# In recent years, Python-based image processing models have 
+# become powerful tools in healthcare, especially for diagnosing
+# skin cancer. These models use computer algorithms to analyze 
+# medical images, helping doctors make more accurate and faster
+# diagnoses. This research report explores how Phyton, and image
+# processing are making a difference in healthcare, focusing on 
+# their role in detecting skin cancer and improving patient care.
+
+# Methodology
+# We will review academic papers, research studies, and cases 
+# that discuss Python-based image processing models in healthcare, 
+# with a focus on skin cancer diagnosis. By analyzing these 
+# sources, we hope to uncover important insights into how these 
+# models are changing healthcare practices and outcomes.
+
+# Preliminary Research
+# Skin cancer, particularly melanoma, is responsible for 75% of 
+# skin cancer-related deaths (Vijayalakshmi M M. 2019). 
+# However, advancements in image processing have significantly 
+# impacted its diagnosis and treatment. Image processing models
+# consist of three main stages: pre-processing, segmentation, 
+# and feature extraction. These stages help reduce the time 
+# taken to diagnose skin cancer and increase the accuracy of 
+# the diagnosis.
+# One of the key benefits of using image processing models 
+# their impact on early detection. In case of stage one skin 
+# cancer, the survival rate is almost 100%, reaching 99.9%. 
+# this highlights the critical role of early detection in 
+#improving patient outcomes.
+# Additionally recent studies by G Bizel (2024) have 
+# highlighted the positive impact of increased machine 
+# learning methods on patient outcomes, not only in skin 
+# cancer but also in other skin diseases. Machine learning 
+# algorithms can classify images and gather patient- related 
+# data, enabling the detection of skin diseases during analysis.
